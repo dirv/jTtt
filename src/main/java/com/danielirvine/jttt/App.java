00feee657594527828ef86abbb124fbc56a564b5
Daniel Irvine <dirvine@8thlight.com>
2014-09-08 13:59:03 +0100

    initial commit of java project with buildr

---
+++ src/main/java/com/danielirvine/jttt/App.java
@@ -0,0 +1,13 @@
+package com.danielirvine.jttt;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
